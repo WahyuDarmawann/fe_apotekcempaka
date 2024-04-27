53ccfb296da065c6a64f69184106b34f6b79da86
aryamahendr4 <adityaaryaa22@gmail.com>
2024-04-27 18:27:24 +0800

    buat isi produk dikit dikit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import apotekcempakaFooter from '../assets/apotekcempaka_footer.svg'
 function Footer(){
     return(
         <React.Fragment>
-            <footer className="min-h-64 py-7 px-10 bg-primari">
+            <footer className="min-h-96 py-10 px-10 bg-primari bg-[url('/src/assets/bg-footer.svg')]">
                     <div className="flex justify-between items-center">
                         <div>
                             <img src={apotekcempakaFooter}></img>
@@ -17,6 +17,9 @@ function Footer(){
                             <li className="px-4 text-sekunder"><a href="#">Produk</a></li>
                             <li className="px-4 text-sekunder"><a href="#">Kontak</a></li>
                         </div>
+                    </div>
+                    <div class="flex justify-center items-center text-sekunder">
+                        <p>&copy; 2024 Apotek Cempaka. Hak cipta dilindungi.</p>
                     </div>
             </footer>
         </React.Fragment>
--- src/pages/Produk.js
+++ src/pages/Produk.js
@@ -1,13 +1,89 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBillWave } from "@fortawesome/free-solid-svg-icons";
+import fotoProduk from "../assets/contoh_obat.jpg";
 
 function Produk() {
     return (
         <React.Fragment>
             <Header/>
-
-            Ini ngambil dari Produk
+            <section className="min-h-screen">
+                <div className="mx-20 py-10 grid grid-cols-5 gap-5">
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
             <Footer/>
         </React.Fragment>
     )
