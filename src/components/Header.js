a0579a70e5f9205ce6eb3ba8da56d821801a3570
WahyuDarmawann <gerald.kelana@gmail.com>
2024-04-27 13:52:04 +0800

    test koneksi

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import apotekcempaka from '../assets/apotekcempaka.svg';
 
 function Header(){
     return(
         <React.Fragment>
             <nav className="py-7 px-10 bg-blue-600">
-                <div className="container ">
-                    <img src={apotekcempaka} alt="" />
-                </div>
+                <div className="flex justify-between items-center">
+                    <div className="nav-image">
+                        <img src={apotekcempaka} alt="" />
+                    </div>
+                    <div className="nav-navigation flex list-none">
+                        <li className="px-4"><a href="#">Beranda</a></li>
+                        <li className="px-4"><a href="#">Profil</a></li>
+                        <li className="px-4"><a href="#">Produk</a></li>
+                        <li className="px-4"><a href="#">Kontak</a></li>
+                    </div>
+                    <div className="login_logo">
+                        <FontAwesomeIcon icon={faArrowRightToBracket} />
+                    </div>
+                </div>  
             </nav>
         </React.Fragment>
     );
