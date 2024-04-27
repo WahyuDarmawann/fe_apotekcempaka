f78118067073f3e4dedfc1f0e79091db0d39a9a1
WahyuDarmawann <gerald.kelana@gmail.com>
2024-04-27 15:16:31 +0800

    buat navigasi ke setiap halaman

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Profil from "./pages/Profil";
+import Produk from "./pages/Produk";
+import Kontak from "./pages/Kontak";
 
 function App() {
   return (
-    <div className='App'>
-      <div>
-        <Header/>
-      </div>
-      <div>
-        <Footer/>
-        <p>Janvukkkk</p>
-      </div>  
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profil" element={<Profil/>}/>
+          <Route path="/produk" element={<Produk/>}/>
+          <Route path="/kontak" element={<Kontak/>}/>
+        </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import apotekcempaka from '../assets/apotekcempaka.svg';
+import { useNavigate } from "react-router-dom";
 
 function Header(){
+    const navigasi = useNavigate()
     return(
         <React.Fragment>
             <nav className="py-7 px-10 bg-blue-600">
@@ -12,10 +14,10 @@ function Header(){
                         <img src={apotekcempaka} alt="logo apotek" />
                     </div>
                     <div className="nav-navigation flex list-none">
-                        <li className="px-4"><a href="#">Beranda</a></li>
-                        <li className="px-4"><a href="#">Profil</a></li>
-                        <li className="px-4"><a href="#">Produk</a></li>
-                        <li className="px-4"><a href="#">Kontak</a></li>
+                        <li className="px-4"><a href="/">Beranda</a></li>
+                        <li className="px-4"><a href="/profil">Profil</a></li>
+                        <li className="px-4"><a href="/produk">Produk</a></li>
+                        <li className="px-4"><a href="/kontak">Kontak</a></li>
                     </div>
                     <div className="login_logo">
                         <FontAwesomeIcon icon={faArrowRightToBracket} />
