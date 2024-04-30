e52fde1fa5aaf0349347bc36cb530ea1f407f67b
aryamahendr4 <adityaaryaa22@gmail.com>
2024-04-30 10:37:01 +0800

    komit mau liat kontak

--- src/pages/Produk.js
+++ src/pages/Produk.js
@@ -111,6 +111,16 @@ function Produk() {
                             <p className="text-right">Stok <span className="font-bold">54</span></p>
                         </div>
                     </div>
+                    <div className="h-70">
+                        <div className="">
+                                <img className="w-full max-w-60 rounded-t-lg" src={fotoProduk}></img>
+                        </div>
+                        <div className="bg-primari text-sekunder px-3 py-2 rounded-b-lg w-full max-w-60">
+                            <p>Konidin Tablet</p>
+                            <p className="text-xl font-bold"><FontAwesomeIcon className="mr-2" icon={faMoneyBillWave} />Rp 3.000</p>
+                            <p className="text-right">Stok <span className="font-bold">54</span></p>
+                        </div>
+                    </div>
                 </div>
             </section>
             <Footer/>
